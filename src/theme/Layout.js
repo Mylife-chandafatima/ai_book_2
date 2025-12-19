11da12dd393e21d569f3c5375c2a9ea1d7c21fc0
Mylife-chandafatima <zubiafatima99@gmail.com>
2025-12-19 22:22:12 +0500

    Fix white page issue by removing AuthProvider from Layout.js

--- src/theme/Layout.js
+++ src/theme/Layout.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import OriginalLayout from '@theme-original/Layout';
-import ChatbotWidget from '@site/src/components/ChatbotWidget/ChatbotWidget';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import LocaleNavigationTop from '@site/src/components/LocaleNavigationTop';
+import ChatbotWidget from '@site/src/components/Chatbot/ChatbotWidget';
 
 export default function Layout(props) {
-  const { siteConfig } = useDocusaurusContext();
-
-  // Since Docusaurus runs client-side after hydration,
-  // we can safely check if we're in the browser environment
-  const [showChatbot, setShowChatbot] = React.useState(false);
-  const [showTranslator, setShowTranslator] = React.useState(false);
-
-  React.useEffect(() => {
-    // Enable chatbot in all environments to make it visible
-    setShowChatbot(true);
-    // The translator is now on its own page, so we don't show it here
-    setShowTranslator(false);
-  }, []);
-
   return (
-    <>
-      <OriginalLayout {...props} />
-      {/* Show chatbot on all pages */}
-      {showChatbot && <ChatbotWidget />}
-    </>
+    <OriginalLayout {...props}>
+      <LocaleNavigationTop />
+      {props.children}
+      <ChatbotWidget />
+    </OriginalLayout>
   );
 }
